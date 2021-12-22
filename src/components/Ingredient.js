b0258c899a5c0e8f8d743907c40f21e31f24c963
pocketcorp <pocketcorp@protonmail.com>
2021-12-21 22:21:47 -0800

    recommit of previous

---
+++ src/components/Ingredient.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Ingerdient = ({name, amount}) => {
+    return (
+        <>
+            <span>{name} </span>
+             
+            <span>{amount} </span>
+        </>
+    );
+}
+
+export default Ingerdient;
