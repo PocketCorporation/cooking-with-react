74ea6c61d72b5c393a70832c3d014b007422bba2
pocketcorp <pocketcorp@protonmail.com>
2021-12-25 21:39:16 -0800

    right side css

---
+++ src/components/RecipeIngredientEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RecipeIngredientEdit = () => {
+    return (
+        <>
+            <input type="text" className="recipe-edit__input"/>
+            <input type="text"className="recipe-edit__input" />
+            <button className="btn btn--danger">&times;</button>
+        </>
+    );
+}
+
+export default RecipeIngredientEdit;
